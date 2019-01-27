c30a44e42e9b2874acd5d3702f925e61eed09589
gekka <10055381+gekka@users.noreply.github.com>
2019-01-27 17:18:13 +0900

    V1.00

---
+++ CoverageTools/CoverageInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gekka.VisualStudio.Extension.CoverageTools
+{
+    partial class CoverageInfo
+    {
+        public const string CoverageFileExtention = ".coverage";
+        public const string CodecoverageWindowGuidString = "{905DA7D1-18FD-4A46-8D0F-A5FF58ADA9DE}";
+
+        private System.Reflection.MethodInfo miMergeCoverageFile;
+
+        public void Initialize()
+        {
+            if (miMergeCoverageFile != null)
+            {
+                return;
+            }
+
+            var asm = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(_ => _.GetName().Name == "Microsoft.VisualStudio.Coverage.Analysis");
+
+            if (asm == null)
+            {
+                throw new ApplicationException("Coverage Module is not loading.");
+            }
+
+            Type t = asm.GetType("Microsoft.VisualStudio.Coverage.Analysis.CoverageInfo", false);
+            var mi = t?.GetMethod
+                        ("MergeCoverageFiles"
+                        , System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static
+                        , null, new Type[] { typeof(string), typeof(string), typeof(string), typeof(bool) }
+                        , null);
+            if (t == null || mi == null)
+            {
+                throw new ApplicationException("Not found Merge function.");
+            }
+            miMergeCoverageFile = mi;
+        }
+
+        public void MergeCoverageFiles(string firstCoverageFile, string secondCoverageFile, string outputCoverageFile, bool overwriteOutputFile = false)
+        {
+            Initialize();
+
+            miMergeCoverageFile.Invoke(null, new object[] { firstCoverageFile, secondCoverageFile, outputCoverageFile, overwriteOutputFile });
+        }
+
+        public void MergeCoverageFiles(IEnumerable<string> coverageFiles, string outputCoverageFile, bool overwriteOutputFile = false)
+        {
+            Initialize();
+
+            if (coverageFiles.Any(_ => string.Equals(_, outputCoverageFile, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                throw new ArgumentException(nameof(outputCoverageFile));
+            }
+            string firstCoverageFile = coverageFiles.FirstOrDefault();
+            if (firstCoverageFile == null)
+            {
+                return;
+            }
+            System.IO.File.Copy(firstCoverageFile, outputCoverageFile, overwriteOutputFile);
+
+            string[] files = coverageFiles.Skip(1).ToArray();
+            if (files.Length == 0)
+            {
+                return;
+            }
+
+            string temp1 = GetTempFile();
+            string temp2 = GetTempFile();
+
+            try
+            {
+                System.IO.File.Copy(firstCoverageFile, temp1, true);
+                string tempinput;
+                string tempoutput = firstCoverageFile;
+
+                foreach (string file in files)
+                {
+                    tempinput = tempoutput;
+                    tempoutput = tempoutput == temp1 ? temp2 : temp1;
+                    miMergeCoverageFile.Invoke(null, new object[] { tempinput, file, tempoutput, true });
+                }
+                System.IO.File.Copy(tempoutput, outputCoverageFile, overwriteOutputFile);
+            }
+            finally
+            {
+                System.IO.File.Delete(temp1);
+                System.IO.File.Delete(temp2);
+            }
+        }
+
+        private static string GetTempFile()
+        {
+            string temp = System.IO.Path.GetTempFileName();
+            string tempx = System.IO.Path.ChangeExtension(temp, "coverage");
+            System.IO.File.Move(temp, System.IO.Path.Combine(temp, tempx));
+            return tempx;
+        }
+    }
+}
---
+++ CoverageTools/MergeCoverageCommand.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Gekka.VisualStudio.Extension.CoverageTools
+{
+    internal sealed class MergeCoverageCommand
+    {
+        public const int CommandId = PackageIds.MergeCoverageCommandId; //0x0100;
+        public static readonly Guid CommandSet = new Guid(PackageGuids.guidMergeCoverageCommandPackageCmdSetString); //new Guid("dce419bc-d1c0-43b3-ab20-28547a442ece");
+
+        private readonly AsyncPackage package;
+
+        public static MergeCoverageCommand Instance { get; private set; }
+
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get { return this.package; }
+        }
+
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new MergeCoverageCommand(package, commandService);
+        }
+
+        private MergeCoverageCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        private async void Execute(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var dte = await this.ServiceProvider.GetServiceAsync(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            if (dte == null)
+            {
+                return;
+            }
+            try
+            {
+                var w = dte.Windows.Item(CoverageInfo.CodecoverageWindowGuidString);
+                w.Activate();
+            }
+            catch
+            {
+                VsShellUtilities.ShowMessageBox(this.package, "Can't open CodeCoverage Window", "Merge Coverage File", OLEMSGICON.OLEMSGICON_WARNING, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                return;
+            }
+
+            string[] folders;
+            using (var dlg = new Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog())
+            {
+                dlg.Title = "Select folders containg .coverage.";
+                dlg.IsFolderPicker = true;
+                dlg.Multiselect = true;
+                if (dlg.ShowDialog() != Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
+                {
+                    return;
+                }
+
+                folders = dlg.FileNames.ToArray();
+            }
+
+            List<string> files = new List<string>();
+            string output;
+            foreach (string folder in folders)
+            {
+                files.AddRange(System.IO.Directory.GetFiles(folder, "*" + CoverageInfo.CoverageFileExtention, System.IO.SearchOption.TopDirectoryOnly));
+            }
+
+            if (files.Count == 0)
+            {
+                return;
+            }
+
+            using (var dlg = new Microsoft.WindowsAPICodePack.Dialogs.CommonSaveFileDialog())
+            {
+                dlg.Title = "Select output file";
+                dlg.AlwaysAppendDefaultExtension = true;
+                dlg.DefaultExtension = CoverageInfo.CoverageFileExtention;
+                dlg.Filters.Add(new Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogFilter("Coverage File", "*" + CoverageInfo.CoverageFileExtention));
+                dlg.OverwritePrompt = true;
+
+
+                if (dlg.ShowDialog() != Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
+                {
+                    return;
+                }
+                output = dlg.FileName;
+            }
+
+            try
+            {
+                var ci = new CoverageInfo();
+                ci.MergeCoverageFiles(files, output, true);
+                //ci.LoadCoverageFileToVS(output, dte);
+            }
+            catch (Exception ex)
+            {
+                VsShellUtilities.ShowMessageBox(this.package, ex.Message, "Merge Coverage File", OLEMSGICON.OLEMSGICON_WARNING, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                return;
+            }
+        }
+    }
+}
